4ae0e3e37c9206218e367554934a4563fcb1d2d4
lukarostan <lukarostan@gmail.com>
2024-11-23 23:41:35 +0100

    Hover on main buttons

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -12,13 +12,13 @@ type ButtonProps = {
 export default function Button({ title, href = '', onClick, extraClass }: ButtonProps): ReactElement {
   if (typeof href === 'string' && href !== '') {
     return (
-      <a className={clsx(style.customButton, extraClass)} href={href} target="_blank">
+      <a className={clsx(style.customButton, style.clickable, extraClass)} href={href} target="_blank">
         {title}
       </a>
     );
   }
   return (
-    <button className={clsx(style.customButton, extraClass)} onClick={onClick}>
+    <button className={clsx(style.customButton, style.clickable, extraClass)} onClick={onClick}>
       {title}
     </button>
   );
